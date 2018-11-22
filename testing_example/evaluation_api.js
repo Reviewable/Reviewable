a4cf30065166d6a548251f67ffe67bb092a7ef22
Piotr Kaminski <piotr@ideanest.com>
2018-11-22 11:47:27 -0800

    Update evaluation api example to accept other modules.

--- testing_example/evaluation_api.js
+++ testing_example/evaluation_api.js
@@ -1,5 +1,9 @@
 'use strict';
 
 exports.api = {
-  _: require('lodash')
+  _: require('lodash'),
+  require: function(path) {
+    if (['http', 'https'].includes(path)) return require(path);
+    throw new Error('Module not whitelisted: ' + path);
+  }
 };
