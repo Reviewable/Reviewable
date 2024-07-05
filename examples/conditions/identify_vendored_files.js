d2be082a190db7901b6add4ba06746561a2da670
Alvin <al.charity@gmail.com>
2024-07-05 12:54:53 -0400

    Remove trailing space and add newline at EOF

--- examples/conditions/identify_vendored_files.js
+++ examples/conditions/identify_vendored_files.js
@@ -1,8 +1,8 @@
-// This snippet checks for the existence of a directory called `third_party` used 
-// to store vendored files. The regular expression can be changed to match  
-// any directory where vendored files are placed. 
+// This snippet checks for the existence of a directory called `third_party` used
+// to store vendored files. The regular expression can be changed to match
+// any directory where vendored files are placed.
 // When the `third_party` directory is found, the snippet below will mark
-// all files in this directory as reviewed. 
+// all files in this directory as reviewed.
 
 // dependencies: lodash4
 
@@ -10,7 +10,7 @@ _.forEach(review.files, (file) => {
 
   if (/^third_party$/i.test(file.path)) {
     _.forEach(file.revisions, (rev) => {
-      rev.reviewed = true;
+      file.vendored = true;
     });
   }
 
