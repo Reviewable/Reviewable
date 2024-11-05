09772f3effaa3acaef263542e1d301a07b26d04c
Piotr Kaminski <piotr@ideanest.com>
2024-11-04 16:36:02 -0800

    Fix ToC anchor slug generation when title contains code.

--- docs/.vitepress/config.mjs
+++ docs/.vitepress/config.mjs
@@ -88,7 +88,7 @@ function page(path) {
     const title = match[2].replace(/`(.*?)`/g, '<code>$1</code>');
     const slug =
       match[3] ||
-      title.toLowerCase().replace(/[^a-z0-9\-]/gi, '-').replace(/-+/g, '-').replace(/^-|-$/, '');
+      match[2].toLowerCase().replace(/[^a-z0-9\-]/gi, '-').replace(/-+/g, '-').replace(/^-|-$/, '');
     const item = {text: title, link: `/${path}#${slug}`};
     if (level === currentLevel) {
       itemStack.pop();
